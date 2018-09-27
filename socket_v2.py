7983ffb79373292262985c0f7905a8a700b22a1d
DanielNas2018 <danielnascimentosouza@hotmail.com>
2018-09-27 18:49:42 -0300

    Atualizando projeto Python

--- socket_v2.py
+++ socket_v2.py
@@ -2,18 +2,12 @@ import socket
 
 def main():
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	print(s)
-	server = 'pythonprogramming.net'
+	#print(s)
+	server = input("Informe o dominio que deseja verificar o ip: ")
 	port = 80
 	server_ip = socket.gethostbyname(server)
 	print()
-	print(server_ip)
-	request = "GET/ HTTP/1.1\nHOST: "+server+"\n\n"
-	s.connect((server,port))
-	s.send(request.encode())
-	result = s.recv(4064)
-	print()
-	print(result)
+	print(server_ip)	
 
 if __name__ == '__main__':
 	main()
