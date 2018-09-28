cb3242017657fa044ebcbed2ddadd63b5efd3a1e
DanielNas2018 <danielnascimentosouza@hotmail.com>
2018-09-28 11:11:53 -0300

    Deixando apenas IP

--- socket_v2.py
+++ socket_v2.py
@@ -1,13 +1,14 @@
 import socket
 
 def main():
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	#print(s)
 	server = input("Informe o dominio que deseja verificar o ip: ")
 	port = 80
 	server_ip = socket.gethostbyname(server)
 	print()
-	print(server_ip)	
+	print(server_ip)
 
 if __name__ == '__main__':
 	main()
+
+
+
