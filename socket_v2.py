198f7ea761f756577c4fa809e0b450cc3479770c
DanielNas2018 <danielnascimentosouza@hotmail.com>
2018-09-29 16:07:47 -0300

    Alteração e criação dos metodos e comentarios feito

--- socket_v2.py
+++ socket_v2.py
@@ -1,29 +1,37 @@
-import socket
+import socket #importando socket
 
-class Rede:
-	def IP(self, dominio):	
+class Rede:	#criando classe rede para organização das funções
+
+	def IP(self, dominio):	#função IP, pega o valor enviando pelo o usuario e inseri dentro da variavel dominio
 		self.dominio = dominio	
 				
-	def obterRede(self):
+	def obterRede(self):	#função obterRede, recebe o ip do dominio retorna para o usuario
 		return self.dominio	
 
-	def HOST(self,host):
+	def HOST(self,host):	#função HOST, pega o nome da maquina
 		self.host = host
 		 
-	def obterHost(self):
-		return  self.host
-
+	def obterHost(self):	#função obterHost, envia o nome para o usuario
+		return  self.host	
 	
-dominio = input("Informe o dominio que deseja verificar o ip: ")
-server_ip = socket.gethostbyname(dominio)
-host = socket.gethostname()
+def Dominio():	#Metodo criado para receber os dados do usuario e enviar para a classe Rede
+	dominio = input("Informe o dominio que deseja verificar o ip: ") #recebe o dominio que o usúario digitar
+	server_ip = socket.gethostbyname(dominio)#atráves do comando "gethostbyname" o dominio que o usuario 
+	#digitou, será pego apenas o ip e jogago para dentro da variavel server_ip
+	rede = Rede() #instanciando a classe a Rede
+	rede.IP(server_ip) 	#envia o ip
+	print(rede.obterRede())	#Retorna o ip
+if __name__ =='__main__':	#Parar a aplicação
+	Dominio() #chama a aplicaçao
 
-rede = Rede()
-rede.IP(server_ip)
-print(rede.obterRede())
-rede.HOST(host)
-print("\nNome da sua Maquina: " + rede.obterHost())
-print()
+def nomeMaquina():
+	host = socket.gethostname()
+	rede = Rede()
+	rede.HOST(host)
+	print("\nNome da sua Maquina: " + rede.obterHost())
+	print()	
+if __name__ =='__main__':
+	nomeMaquina()
 
 
 
