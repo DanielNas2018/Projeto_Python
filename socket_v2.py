27baab23b0a68d3243d9e506d1561d43e1b91415
DanielNas2018 <danielnascimentosouza@hotmail.com>
2018-09-28 21:12:52 -0300

    Incluindo o hostname

--- socket_v2.py
+++ socket_v2.py
@@ -1,14 +1,25 @@
 import socket
 
-def main():
-	server = input("Informe o dominio que deseja verificar o ip: ")
-	port = 80
-	server_ip = socket.gethostbyname(server)
-	print()
-	print(server_ip)
 
-if __name__ == '__main__':
-	main()
+
+class Rede():
+	def main():
+		server = input("Informe o dominio que deseja verificar o ip: ")
+		server_ip = socket.gethostbyname(server)
+		print()
+		print(server_ip)
+
+	if __name__ == '__main__':
+		main()
+
+	def IP():
+		server_ip = socket.gethostname()
+		print()
+		print("Nome da Maquina: ",server_ip)
+
+	if __name__ == '__main__':
+		IP()
+
 
 
 
